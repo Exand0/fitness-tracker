3ac41f0ed1aefc1f3cb5de8ca813e97445670a0e
Sascha K <ku3menko.sasha@gmail.com>
2019-09-23 19:20:34 +0200

    refactor

--- data/js/calendar.js
+++ data/js/calendar.js
@@ -1,65 +1,263 @@
-//const body = document.querySelector('body');
-let container = document.createElement('div');
-body.appendChild(container);
+//     // let nextButton = document.createElement('button');
+//     // nextButton.classList.add('control_button');
+//     // let previousButton = document.createElement('button');
+//     // previousButton.classList.add('control_button');
 
-let date = new Date();
-let year = date.getFullYear();
-let monthName = date.toLocaleString('default', {month: 'long'});
-let monthNum = date.getMonth();
-let today = date.getDate();
-let currentDay = new Date(year, monthNum, 1);
-let lastDay = new Date(year, monthNum + 1, 0);
+//     //let year = 2019;
+//     //month 0-11
+//     //let monthNum = 8;
+// // class Days {
+// //     constructor() {
 
-let nextButton = document.createElement('button');
-nextButton.classList.add('control_button');
-let previousButton = document.createElement('button');
-previousButton.classList.add('control_button');
+// //     }
+// // }
 
-let monthLabel = document.createElement('p');
-monthLabel.innerText = monthName;
-monthLabel.classList.add('month_label');
-container.appendChild(monthLabel);
+// // let body = document.querySelector('body');
 
-let tb = document.createElement('table');
-container.appendChild(tb);
-let weekDay;
+// // class Month {
+// //     constructor(year, monthNum) {
+// //         this.year = year;
+// //         this.month = monthNum;
+// //         // this.self = this;
+// //     }
+// //     drawMonth() {
+// //         // console.log(self);
+// //         let container = document.createElement('div');
+// //         let monthLabel = document.createElement('p');
+// //         let tb = document.createElement('table');
+    
+// //         let firstDay = new Date(this.year, this.month, 1);
+// //         let lastDay = new Date(this.year, this.month + 1, 0);
 
-// Making Monday first day of the week
-(currentDay.getDay() === 0) ? weekDay = 6 : (weekDay = currentDay.getDay() - 1);
-
-// Beginning of the month is current day - day of the week
-for (let index = (-1) * weekDay + 1, week = 0, day = 0; index <= lastDay.getDate(); index++, week++, day++) {
-
-    let td = document.createElement('td');
-    td.classList.add('day');
-    if (((day ===  5) || (day ===  6)) && index > 0) {
-        td.classList.add('holiday');
-    }
-
-    if (!(day%7)) {
-        tr = document.createElement('tr');
-        tb.appendChild(tr);
-        day = 0;
-    }
-
-    if (index > 0) {
-        td.innerText = index; 
-    }
-    tr.appendChild(td);
-}
-class Year {
-    constructor(year, monthArr) {
-
-    }
-}
-class Month {
-    constructor(month, dayCount, dayArr) {
+// //         let monthName = firstDay.toLocaleString('default', {month: 'long'});
+// //         let weekDay;
         
-    }
+// //         container.appendChild(monthLabel);  
+// //         monthLabel.innerText = monthName;
+// //         monthLabel.classList.add('month_label');
+    
+// //         container.appendChild(tb);
+// //         // setting monday to be the first day
+// //         (firstDay.getDay() === 0) ? weekDay = 6 : (weekDay = firstDay.getDay() - 1);
+// //         let tr;
+// //         for (let index = (-1) * weekDay + 1, day = 0; index <= lastDay.getDate(); index++, day++) {
+// //             let td = document.createElement('td');
+// //             td.classList.add('day');
+// //             //console.log(td);
+
+// //             if (((day ===  5) || (day ===  6)) && index > 0) {
+// //                 td.classList.add('holiday');
+// //             }
+    
+// //             if (!(day%7)) {
+// //                 tr = document.createElement('tr');
+// //                 tb.appendChild(tr);
+// //                 day = 0;
+// //             }
+    
+// //             if (index > 0) {
+// //                 td.innerText = index; 
+// //                 td.id = index;
+// //             }
+// //             tr.appendChild(td);
+// //         }
+// //         return container;
+// //     }
+
+// //     // addField(ul) {
+// //     //     const entry_li = docuemtn.createElement('li');
+// //     // }
+
+
+// //     addEntry(target) {
+// //         const li = document.createElement('li');
+// //         const input = docuemtn.createElement('input');
+// //         input.setAttribute('type', 'text');
+// //         li.classList.add('entry_li');
+// //         target.appendChild(li);
+// //         li.appendChild(input);    
+// //     }
+    
+// //     openDay() {
+// //         let dayCont;
+// //         const addEntryButton = document.createElement('button');
+// //         addEntryButton.classList.add('add_entry');
+// //         addEntryButton.innerText = 'Add entry';
+
+// //         if (!(dayCont = document.querySelector('.day_container'))) {
+// //             //console.log(entry_td);
+// //             dayCont = document.createElement('div');
+// //             dayCont.classList.add('day_container');
+// //             body.appendChild(dayCont);
+// //         } else {
+// //             dayCont.innerHTML = '';
+// //         }
+
+// //         dayCont.appendChild(addEntryButton);
+
+// //         const entry_td = document.createElement('table');
+// //         entry_td.classList.add('entry_td');
+
+// //         const entry_ul = document.createElement('ul');
+// //         entry_ul.classList.add('entry_ul');
+
+// //         dayCont.appendChild(entry_td);
+// //         entry_td.appendChild(entry_ul);
+
+// //         return entry_ul;
+// //     }
+
+
+// //     handleClick(e) {
+// //         let entry_ul;
+// //         const element = e.target;
+// //         if (element.className === 'day') {
+// //             entry_ul = this.openDay();
+// //         } else if (element.className === 'add_entry') {
+// //             console.log(element);
+// //             this.addEntry(entry_ul);
+// //         }
+// //     }
+// // }
+
+// // class Year {
+// //     constructor(year) {
+// //         this.year = year;
+// //     }
+// //     drawYear() {
+// //         //console.log(this);
+// //         for (let i = 0; i < 12; i++) {
+// //             let month = new Month(this.year, i);
+// //             let monthCont = month.drawMonth();
+// //             monthCont.classList.add('month');
+// //             body.appendChild(monthCont);
+// //             monthCont.addEventListener('click', e => month.handleClick(e));
+
+// //         }
+// //     }
+
+// // }
+
+// // let year = (new Year(2019)).drawYear();
+
+// //-----------------
+
+// class Day {
+//     constructor(weekDayNum, dayNum, isWeekend) {
+//         this.entryArr = [];
+//         this.weekDayNum = weekDayNum;
+//         this.dayNum = dayNum;
+//         this.isWeekend = isWeekend;
+//         //this.targetEl;
+//     }
+
+//     addEntry(entryData) {
+//         //console.log(this);
+//         this.entryArr.push(entryData);
+//         drawContainer();
+//     }
+//     handleClick(e) {
+//         if (e.target.className === 'delete_button') {
+//             //console.log(e.target.classList);
+//             this.deleteEntry(e.target);
+//             tdrawContainer();
+//         } else if (e.target.className === '.entry_li') {
+//             this.updateEntry(e.target);
+//             drawContainer();
+//         }
+
+//     }
+//     deleteEntry(target) {
+//         const id = target.parentNode.id;
+//         this.entryArr.splice(id, 1);
+//         console.log('delete');
+//     }
+//     updateEntry(target) {
+
+//     }
+//     // getEntry(index) {
+//     //     if (this.entryArr.length !== 0) {
+//     //         return this.entryArr.at(index);
+//     //     }
+//     // }
+//     getEntryArr() {
+//         return this.entryArr;
+//     }
+//     // getArrSize() {
+//     //     return this.entryArr.length;
+//     // }
+// }
+
+// const container = document.createElement('div');
+// const inputField = document.createElement('input');
+// const inputButton = document.createElement('button');
+// let day = new Day(0, 16, false);
+// let daysArr;
+
+// inputField.setAttribute('type', 'text');
+// inputField.classList.add('input_field');
+// inputButton.classList.add('input_button');
+// container.classList.add('container');
+// inputButton.innerText = 'Add';
+
+// body.appendChild(container);
+// container.appendChild(inputField);
+// container.appendChild(inputButton);
+
+
+// //day.attachUiTo(container);
+// inputButton.addEventListener('click', () => {
+//     let val = inputField.value;
+//     day.addEntry(val);   
+// });
+
+
+function drawContainer(obj) {
+    let entryArr = 
+    container.innerHTML = '';
+    let table = document.createElement('table');
+    let ul = document.createElement('ul');
+    this.entryArr.forEach((entry, index) => {
+        const li = document.createElement('li');
+        const button = document.createElement('button');
+        button.innerText = 'Delete';
+        button.classList.add('delete_button');
+        li.classList.add('entry_li');
+        li.innerText = entry;
+        li.id = index;
+        ul.appendChild(li);  
+        li.appendChild(button);
+    });
+    table.appendChild(ul);
+    container.appendChild(table);
 }
 
-class Day {
-    constructor(data) {
-        this.data = data;
-    }
-}
+// container.addEventListener('click', e => day.handleClick(e));
+   
+//     // class Year {
+//     //     constructor(year, monthArr) {
+    
+//     //     }
+//     // }
+//     // class Month {
+//     //     constructor(year, month, dayCount, dayArr) {
+//     //         this.year = year;
+//     //         this.month = month;
+//     //         this.dayCount = dayCount;
+//     //         this.dayArr = dayArr
+//     //     }
+//     // }
+    
+//     // class Day {
+//     //     constructor(data, day, weekDay) {
+//     //         this.data = data;
+//     //         this.day = day;
+//     //         this.weekDay = weekDay;
+//     //     }
+//     //     getDayElement() {
+    
+//     //     }
+//     // }
+    
+//     // let sept = new Month(year, monthNum);
+//     // body.appendChild(sept.drawMonth());
--- data/js/core.js
+++ data/js/core.js
@@ -1,68 +1,231 @@
-const body = document.querySelector('body');
-const inputField = document.querySelector('.input');
-const inputButton = document.querySelector('.button_weight');
-const entryList = document.createElement('ul');
-entryList.classList.add('ulist_entries');
-body.appendChild(entryList);
+const container = document.querySelector('.container');
+const outputCont = document.createElement('div');
+const calendarCont = document.createElement('div');
+const inputField = document.createElement('input');
+const inputButton = document.createElement('button');
+inputButton.classList.add('input_button');
+calendarCont.classList.add('calendar_container')
+outputCont.classList.add('output_container');
+inputButton.innerText = 'Add';
 
-let valArr = [];
+container.appendChild(inputField);
+container.appendChild(inputButton);
+container.appendChild(outputCont);
+container.appendChild(calendarCont);
 
-function handleInput(e) {
-    if ((e.keyCode == "13") || (e.target.className == 'button_weight')) {
-        const inputVal = inputField.value;
-        addEntry(inputVal);
-        displayEntries(inputVal);
+
+class Day {
+    constructor(weekDayNum, dayNum, isWeekend = false) {
+        this.entryArr = [];
+        this.weekDayNum = weekDayNum;
+        this.dayNum = dayNum;
+        this.isWeekend = isWeekend;
+    }
+    addEntry(entryData) {
+        this.entryArr.push(entryData);
+    }
+    deleteEntry(target) {
+        const id = target.parentNode.id;
+        this.entryArr.splice(id, 1);
+    } 
+    updateEntry(position, value) {
+        this.entryArr.splice(position, 1, value);
+    }
+    getEntry(position) {
+        return this.entryArr[position];
+    }
+    getEntryArr() {
+        return this.entryArr;
     }
 }
 
-function addEntry(value) {
-    valArr.push(value);
-}
+let day = new Day(1, 2);
 
-function createDeleteButton() {
-    const delButton = document.createElement('button');
-    delButton.classList.add('button_delete');
-    delButton.innerText = 'Delete';
-    return delButton;
-}
+container.addEventListener('click', e => handleClick(e));
+let indexCache;
 
-function displayEntries(value) {
-    entryList.innerHTML = '';
-    valArr.forEach((value, pos) => {
+function drawOutput() {
+    outputCont.innerHTML = '';
+    let table = document.createElement('table');
+    let ul = document.createElement('ul');
+    let entryArr = day.getEntryArr();
+    entryArr.forEach((entry, index) => {
         const li = document.createElement('li');
-        li.id = pos;
-        li.innerText = value;
-        //console.log(li);
-        li.classList.add('li_entry');
-        entryList.appendChild(li);
-        li.appendChild(createDeleteButton());
+        const button = document.createElement('button');
+        button.innerText = 'Delete';
+        button.classList.add('delete_button');
+        li.classList.add('entry_li');
+        li.innerText = entry;
+        li.id = index;
+        ul.appendChild(li);  
+        li.appendChild(button);
     });
-    entryList.addEventListener('click', deleteEntry);
-    //entryList.addEventListener('keydown', updateEntry);
+    table.appendChild(ul);
+    outputCont.appendChild(table);
+}
+
+function toggleButton() {
+    if (inputButton.className === 'update_button') {
+        inputButton.innerText = 'Add';
+    } else if (inputButton.className === 'input_button') {
+        inputButton.innerText = 'Update';
+    }
+    inputButton.classList.toggle('input_button');
+    inputButton.classList.toggle('update_button');
 
 }
 
-// function updateEntry(element) {
-//     if (element.target.className === 'li_entry') {
-//         const id = element.target.parentNode.id;
-//         //valArr.splice(id, 1, element.target.innerText);
-//         displayEntries();
-//         console.log(element.target.innerText);
+function handleClick(e) {
+    if (e.target.className === 'delete_button') {
+        //console.log(e.target.classList);
+        day.deleteEntry(e.target);
+    } else if (e.target.className === 'entry_li') {
+        indexCache = e.target.id;
+        let  val = day.getEntry(indexCache);
+        inputField.value = val;
+        toggleButton();
+    } else if (e.target.className === 'input_button') {
+        day.addEntry(inputField.value);
+        inputField.value = '';
+    } else if (e.target.className === 'update_button') {
+        day.updateEntry(indexCache, inputField.value);
+        toggleButton();
+        inputField.value = '';
+        indexCache = '';
+    }
+    drawOutput();
+}
+
+class Month {
+    constructor(year, monthNum) {
+        this.year = year;
+        this.monthNum = monthNum;
+        this.firstDay = new Date(this.year, this.monthNum - 1, 1);
+        this.lastDay = new Date(this.year, this.monthNum, 0);
+        this._firstWeekDay;
+        this.dayArr = [];
+    }
+    set firstWeekDay(day) {
+        this._firstWeekDay = ((this.firstDay.getDay() + 7 - day) % 7);
+    }
+    get firstWeekDay() {
+        return this._firstWeekDay;
+    }
+    get monthName() {
+        return this.firstDay.toLocaleString('default', {month: 'long'});
+    }
+    get dayCount() {
+        return this.lastDay.getDate();
+    }
+    buildMonth() {
+        for (let i = 1; i <= this.dayCount; i++ ) {
+            // const day = new Day(1, i);
+            // day.getDay() ? 
+            this.dayArr.push(new Day(1, i));
+        }
+    }
+}
+
+function drawMonth() {
+
+    const monthCont = document.createElement('div');
+    let monthLabel = document.createElement('p');
+    let tb = document.createElement('table');
+
+    monthCont.appendChild(monthLabel);  
+    monthCont.appendChild(tb);
+    monthLabel.innerText = month.monthName;
+    monthLabel.classList.add('month_label');
+
+    let tr;
+    for (let index = (-1) * month.firstWeekDay + 1, day = 0; index <= month.dayCount; index++, day++) {
+        let td = document.createElement('td');
+        td.classList.add('day');
+
+        if (((day ===  5) || (day ===  6)) && index > 0) {
+            td.classList.add('holiday');
+            month.dayArr[index].isWeekend = true;
+        }
+
+        if (!(day%7)) {
+            tr = document.createElement('tr');
+            tb.appendChild(tr);
+            day = 0;
+        }
+
+        if (index > 0) {
+            td.innerText = index; 
+            td.id = index;
+        }
+        tr.appendChild(td);
+    }
+    return monthCont;
+}
+
+let month = new Month(2019, 9);
+
+month.firstWeekDay = 1;
+drawOutput();
+month.buildMonth();
+calendarCont.appendChild(drawMonth());
+
+// class Month {
+//     constructor(year, monthNum) {
+//         this.year = year;
+//         this.month = monthNum;
+//     }
+
+//     // addField(ul) {
+//     //     const entry_li = docuemtn.createElement('li');
+//     // }
+
+//     addEntry(target) {
+//         const li = document.createElement('li');
+//         const input = docuemtn.createElement('input');
+//         input.setAttribute('type', 'text');
+//         li.classList.add('entry_li');
+//         target.appendChild(li);
+//         li.appendChild(input);    
+//     }
+    
+//     openDay() {
+//         let dayCont;
+//         const addEntryButton = document.createElement('button');
+//         addEntryButton.classList.add('add_entry');
+//         addEntryButton.innerText = 'Add entry';
+
+//         if (!(dayCont = document.querySelector('.day_container'))) {
+//             //console.log(entry_td);
+//             dayCont = document.createElement('div');
+//             dayCont.classList.add('day_container');
+//             body.appendChild(dayCont);
+//         } else {
+//             dayCont.innerHTML = '';
+//         }
+
+//         dayCont.appendChild(addEntryButton);
+
+//         const entry_td = document.createElement('table');
+//         entry_td.classList.add('entry_td');
+
+//         const entry_ul = document.createElement('ul');
+//         entry_ul.classList.add('entry_ul');
+
+//         dayCont.appendChild(entry_td);
+//         entry_td.appendChild(entry_ul);
+
+//         return entry_ul;
+//     }
+
+
+//     handleClick(e) {
+//         let entry_ul;
+//         const element = e.target;
+//         if (element.className === 'day') {
+//             entry_ul = this.openDay();
+//         } else if (element.className === 'add_entry') {
+//             console.log(element);
+//             this.addEntry(entry_ul);
+//         }
 //     }
 // }
-
-function deleteEntry(element) {
-    //element.target.parentNode.removeChild(element.target);
-    if (element.target.className === 'button_delete') {
-        const id = element.target.parentNode.id;
-        valArr.splice(id, 1);
-        displayEntries();
-    }
-    // if (element.target.className === 'li_entry') {
-    //     //console.log(element);
-    //     element.target.setAttribute('contentEditable', 'true');
-    // }
-}
-
-inputButton.addEventListener('click', handleInput);
-document.addEventListener('keydown', handleInput);
