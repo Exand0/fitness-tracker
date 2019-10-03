833526d954de2e2dc80aad07287bd0d8e0ddaf8f
Sascha K <ku3menko.sasha@gmail.com>
2019-10-03 12:19:14 +0200

    basic css added

--- data/js/core.js
+++ data/js/core.js
@@ -5,31 +5,19 @@ const monthTable = document.querySelector('.month_table');
 const monthLabel = document.querySelector('.month_label');
 const yearLabel = document.querySelector('.year_label');
 const dayCont = document.querySelector('.day_container');
+const dayEntries = document.querySelector('.day_entries');
 const inputField = document.querySelector('.input_field');
 const inputButton = document.querySelector('.input_button');
 
 let yearArr = Array(12);
 
-for (let i = 0; i < 12; i++)  {
+console.log((new Date()).getMonth());
+console.log(new Date(2019, 11, 1));
+
+for (let i = 0; i < 12; i++) {
     yearArr[i] = Array(31);
 }
 
-let indexCache;
-
-
-
-
-
-function toggleButton() {
-    if (inputButton.className === 'update_button') {
-        inputButton.innerText = 'Add';
-    } else if (inputButton.className === 'input_button') {
-        inputButton.innerText = 'Update';
-    }
-    inputButton.classList.toggle('input_button');
-    inputButton.classList.toggle('update_button');
-
-}
 
 class Day {
     constructor(dayNum) {
@@ -43,8 +31,7 @@ class Day {
     deleteEntry(target) {
         const id = target.parentNode.id;
         this.entryList.splice(id, 1);
-
-    } 
+    }
     updateEntry(position, value) {
         this.entryList.splice(position, 1, value);
     }
@@ -53,50 +40,46 @@ class Day {
 let calendar = {
     year: (new Date()).getFullYear(),
     monthNum: (new Date()).getMonth(),
-    dayCache: 0,
-    setYear: function(year) {
+    dayCache: '',
+    indexCache: '',
+    dayIsActive: false,
+    setYear: function (year) {
         this.year = year;
     },
-    getFirstDay: function() {
-        return new Date(this.year, this.monthNum - 1, 1);
+    getFirstDay: function () {
+        return new Date(this.year, this.monthNum, 1);
     },
-    getLastDay: function() {
-        return new Date(this.year, this.monthNum, 0);
+    getLastDay: function () {
+        return new Date(this.year, this.monthNum + 1, 0);
     },
-    getMonthName: function() {
-        return this.getFirstDay().toLocaleString('default', {month: 'long'});
+    getMonthName: function () {
+        return this.getFirstDay().toLocaleString('default', { month: 'long' });
     },
-    getDayCount: function() {
+    getDayCount: function () {
         return this.getLastDay().getDate();
     },
     setFirstWeekDay: function (day) {
         return ((this.getFirstDay().getDay() + 7 - day) % 7);
     },
-    initMontArr: function() {
-        const days = this.getDayCount();
-        monthArr = Array(days);
-    },
     drawMonth: function () {
-        //this.initMontArr();
         monthTable.innerHTML = '';
         monthTable.id = this.monthNum;
         monthLabel.innerText = this.getMonthName();
-        yearLabel.innerText = (new Date).getFullYear();
+        yearLabel.innerText = this.year;
         let tr;
-        for (let index = (-1) * this.setFirstWeekDay(1) + 1, day = 0; index <= this.getDayCount(); index++, day++) {
+        for (let index = (-1) * this.setFirstWeekDay(1) + 1, day = 0; index <= this.getDayCount(); index++ , day++) {
             let td = document.createElement('td');
-            if (((day ===  5) || (day ===  6)) && index > 0) {
+            if (((day === 5) || (day === 6)) && index > 0) {
                 td.classList.add('holiday');
-                //monthObj.dayArr[index - 1].isWeekend = true;
             }
-            if (!(day%7)) {
+            if (!(day % 7)) {
                 tr = document.createElement('tr');
                 monthTable.appendChild(tr);
                 day = 0;
             }
             if (index > 0) {
                 td.classList.add('day');
-                td.innerText = index; 
+                td.innerText = index;
                 td.id = index;
             }
             tr.appendChild(td);
@@ -105,15 +88,13 @@ let calendar = {
     },
     openDay: function (id) {
         let day;
-        //console.log(yearArr[this.monthNum]);
-        if (typeof (yearArr[this.monthNum - 1][id]) !== 'object' ) {
+        if (typeof (yearArr[this.monthNum][id]) !== 'object') {
             day = new Day(id);
-            yearArr[this.monthNum - 1][id] = day;
+            yearArr[this.monthNum ][id] = day;
         } else {
-            day = yearArr[this.monthNum - 1][id];
+            day = yearArr[this.monthNum ][id];
         }
-        //this.dayCache = day;
-        dayCont.innerHTML = '';
+        dayEntries.innerHTML = '';
         let table = document.createElement('table');
         let ul = document.createElement('ul');
         day.entryList.forEach((entry, index) => {
@@ -124,56 +105,85 @@ let calendar = {
             li.classList.add('entry_li');
             li.innerText = entry;
             li.id = index;
-            ul.appendChild(li);  
+            ul.appendChild(li);
             li.appendChild(button);
         });
         table.appendChild(ul);
-        dayCont.appendChild(table);
+        dayEntries.appendChild(table);
     },
-    handleClick: function(e) {
+    handleClick: function (e) {
         const className = e.target.className;
-        let day = yearArr[this.monthNum - 1][this.dayCache];
+        let day = yearArr[this.monthNum][this.dayCache.id];
         if (className === 'delete_button') {
             day.deleteEntry(e.target);
-            this.openDay(this.dayCache);
+            this.openDay(this.dayCache.id);
         } else if (className === 'entry_li') {
-            //indexCache = e.target.id;
-            //let val = day.getEntry(indexCache);
+            this.indexCache = e.target.id;
             inputField.value = day.entryList[e.target.id];
-            toggleButton();
+            this.toggleButton();
         } else if (className === 'input_button') {
-            //let day = yearArr[this.monthNum - 1][this.dayCache];
             day.addEntry(inputField.value);
             inputField.value = '';
-            console.log(yearArr);
-            this.openDay(this.dayCache);
+            this.openDay(this.dayCache.id);
         } else if (className === 'update_button') {
-            day.updateEntry(indexCache, inputField.value);
-            toggleButton();
+            day.updateEntry(this.indexCache, inputField.value);
+            this.toggleButton();
             inputField.value = '';
             indexCache = '';
-            this.openDay(e.target);
-        // contains class
+            this.openDay(this.dayCache.id);
+            // contains class
         } else if (e.target.classList.contains('day')) {
-            this.dayCache = e.target.id;
-            this.openDay(this.dayCache);
-        } else if (className === 'button_next') {
-            if (calendar.monthNum === 12) {
-                calendar.monthNum = 1;
+            if (!this.dayIsActive) {
+                this.dayIsActive = true;
+                e.target.classList.add('active');
+                dayCont.classList.remove('inactive');  
+            } else {
+                if (this.dayCache.id != e.target.id) {
+                    e.target.classList.add('active');
+                    this.dayCache.classList.remove('active');   
+                } else {
+                    this.dayIsActive = false;
+                    e.target.classList.remove('active');
+                    dayCont.classList.add('inactive'); 
+                }    
+            }
+            this.dayCache = e.target;
+            this.openDay(this.dayCache.id);
+        } else if (className === 'button_next month') {
+            if (calendar.monthNum === 11) {
+                calendar.monthNum = 0;
                 calendar.year++;
             } else {
                 calendar.monthNum++;
             }
+            console.log(calendar.year);
+            console.log(calendar.monthNum);
             this.drawMonth();
-        } else if (className === 'button_previous') {
-            if (calendar.monthNum === 1) {
-                calendar.monthNum = 12;
+        } else if (className === 'button_previous month') {
+            if (calendar.monthNum === 0) {
+                calendar.monthNum = 11;
                 calendar.year--;
             } else {
                 calendar.monthNum--;
             }
-            calthis.endar.drawMonth();
+            this.drawMonth();
+        } else if (className === 'button_next year') {
+            calendar.year++;
+            this.drawMonth();
+        } else if (className === 'button_previous year') {
+            calendar.year--;
+            this.drawMonth();
         }
+    },
+    toggleButton: function() {
+        if (inputButton.className === 'update_button') {
+            inputButton.innerText = 'Add';
+        } else if (inputButton.className === 'input_button') {
+            inputButton.innerText = 'Update';
+        }
+        inputButton.classList.toggle('input_button');
+        inputButton.classList.toggle('update_button');
+    
     }
 }
 
